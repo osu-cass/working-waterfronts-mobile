73c7889273085ec9aaf56af658a81cf08d3894e4
Free-BSG <michael.freeman@oregonstate.edu>
2014-09-22 10:23:17 -0700

    Fix siesta testing errors
    I fixed the testing errors regarding the toolbar tests for the list and
    detail pages.

--- tests/siesta/ui_tests/list_test.t.js
+++ tests/siesta/ui_tests/list_test.t.js
@@ -5,7 +5,7 @@ StartTest(function(t) {
     var tool = Ext.ComponentQuery.query('toolbar');
     // console.log('This is the toolbar: ');
     console.log(tool);
-    t.is(tool[2].config.itemId, 'listPageToolbar', "We have a Header"); 
+    t.is(tool[1].config.itemId, 'listPageToolbar', "We have a Header"); 
 
     // Back button test
     var Bbtn = Ext.ComponentQuery.query('button[itemId=backHomeButton]');
