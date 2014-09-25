f75e79c9174fa105b5db72e48e271bbcdbea9eba
Jacob Broderick <schmiddy373@gmail.com>
2014-09-25 11:16:04 -0700

    Add store tests
    Add the store tests and implement mocking.

--- tests/jasmine-standalone/spec/javascripts/store/productsSpec.js
+++ tests/jasmine-standalone/spec/javascripts/store/productsSpec.js
@@ -4,7 +4,8 @@ describe('SeaGrant_Proto.store.Product',function() {
 
     beforeEach(function() {
         //Set up globals
-        expect(store).toBeUndefined();
+		jasmine.Ajax.install();
+		store = Ext.create('SeaGrant_Proto.store.Product');
     });
 
     afterEach(function() {
@@ -12,15 +13,124 @@ describe('SeaGrant_Proto.store.Product',function() {
 
         //This looks like it should be leaky, check the sencha API for
         //something like the model.destroy() function.
+		jasmine.Ajax.uninstall();
         store = null;
     });
 
 
     it('exists', function() {
-        store = Ext.create('SeaGrant_Proto.store.Product');
         expect(store.$className).toEqual('SeaGrant_Proto.store.Product');
     });
 
+	it('calls out to the proper url', function() {
+		store.load();
+		var request = jasmine.Ajax.requests.mostRecent();
+		expect(request.url).toEqual('/newProducts.json');
+	});
+
+	it('loads data from an api', function(){
+		store.load();
+		var mockedRequest = jasmine.Ajax.requests.mostRecent();
+		mockedRequest.response({
+			status: 200,
+			responseText:[
+			{
+			origin: 'TEST',
+            available: false,
+            description: 'This product is merely conceptual in nature and cannot be purchased.',
+            variety: 'testing',
+            season: 'year-round',
+            image: null,
+            created: 'at testing time',
+            modified: 'never',
+            market_price: 0,
+            link: null,
+            alt_name: 'Pretend Product, Elaborate Ruse',
+            story_id: null,
+            name: 'Test Product Instance'
+			}
+		]
+        });
+		expect(store.getCount()).toEqual(1);
+		expect(store.getAt(0).get('origin')).toEqual('TEST');
+		expect(store.getAt(0).get('available')).toEqual(false);
+		expect(store.getAt(0).get('description')).toEqual('This product is merely conceptual in nature and cannot be purchased.');
+		expect(store.getAt(0).get('variety')).toEqual('testing');
+		expect(store.getAt(0).get('season')).toEqual('year-round');
+		expect(store.getAt(0).get('image')).toEqual(null);
+		expect(store.getAt(0).get('created')).toEqual('at testing time');
+		expect(store.getAt(0).get('modified')).toEqual('never');
+		expect(store.getAt(0).get('market_price')).toEqual(0);
+		expect(store.getAt(0).get('link')).toEqual(null);
+		expect(store.getAt(0).get('name')).toEqual('Test Product Instance');
+    });
+
+	it('loads multiple datas from an api', function(){
+		store.load();
+		var mockedRequest = jasmine.Ajax.requests.mostRecent();
+		mockedRequest.response({
+			status: 200,
+			responseText:[
+			{
+			origin: 'TEST',
+            available: false,
+            description: 'This product is merely conceptual in nature and cannot be purchased.',
+            variety: 'testing',
+            season: 'year-round',
+            image: null,
+            created: 'at testing time',
+            modified: 'never',
+            market_price: 0,
+            link: null,
+            alt_name: 'Pretend Product, Elaborate Ruse',
+            story_id: null,
+            name: 'Test Product Instance'
+			},
+			{
+			origin: 'TEST',
+            available: false,
+            description: 'This product is merely conceptual in nature and cannot be purchased.',
+            variety: 'testing',
+            season: 'year-round',
+            image: null,
+            created: 'at testing time',
+            modified: 'never',
+            market_price: 0,
+            link: null,
+            alt_name: 'Pretend Product, Elaborate Ruse',
+            story_id: null,
+            name: 'Test Product Instance'
+			}
+		]
+        });
+		expect(store.getCount()).toEqual(2);
+		expect(store.getAt(0).get('origin')).toEqual('TEST');
+		expect(store.getAt(0).get('available')).toEqual(false);
+		expect(store.getAt(0).get('description')).toEqual('This product is merely conceptual in nature and cannot be purchased.');
+		expect(store.getAt(0).get('variety')).toEqual('testing');
+		expect(store.getAt(0).get('season')).toEqual('year-round');
+		expect(store.getAt(0).get('image')).toEqual(null);
+		expect(store.getAt(0).get('created')).toEqual('at testing time');
+		expect(store.getAt(0).get('modified')).toEqual('never');
+		expect(store.getAt(0).get('market_price')).toEqual(0);
+		expect(store.getAt(0).get('link')).toEqual(null);
+		expect(store.getAt(0).get('name')).toEqual('Test Product Instance');
+		//data store 2
+		expect(store.getAt(1).get('origin')).toEqual('TEST');
+		expect(store.getAt(1).get('available')).toEqual(false);
+		expect(store.getAt(1).get('description')).toEqual('This product is merely conceptual in nature and cannot be purchased.');
+		expect(store.getAt(1).get('variety')).toEqual('testing');
+		expect(store.getAt(1).get('season')).toEqual('year-round');
+		expect(store.getAt(1).get('image')).toEqual(null);
+		expect(store.getAt(1).get('created')).toEqual('at testing time');
+		expect(store.getAt(1).get('modified')).toEqual('never');
+		expect(store.getAt(1).get('market_price')).toEqual(0);
+		expect(store.getAt(1).get('link')).toEqual(null);
+		expect(store.getAt(1).get('name')).toEqual('Test Product Instance');
+    });
+
+	//Currently not a feature, waiting on getting an api set up. Keeping test
+	//as failure for a reminder.
     it('automatically populates itself with API data', function() {
         store = Ext.create('SeaGrant_Proto.store.Product');
         //Let's test this with sample data.
