23f15b03baa8c1c8e1f05413db8c27177b29d019
jhcarr <Justin.Carr@oregonstate.edu>
2014-10-07 11:03:41 -0700

    Unhide autoload tests and scrub old comments
    Prior to this commit, specs addressing the autoload requirement of
    sencha stores were hidden in comments. While we do not want to derail
    the test process with unimplemented features, we also do not want to
    omit these requirements from the project specifications. This commit
    uses Jasmine's "pending" spec feature to disable these autoload tests
    and preserve them in the overall spec document.
    
    During the development of this branch, comments describing bugs and
    strategies persisted beyond the lifetime of the test code they
    described. Since autoload spec was a member of these comments, this
    commit also removes these comments from all store spec.js files.

--- tests/jasmine-standalone/spec/javascripts/store/educationSpec.js
+++ tests/jasmine-standalone/spec/javascripts/store/educationSpec.js
@@ -2,37 +2,23 @@ describe('SeaGrant_Proto.store.Education',function() {
 
     // Ext.require('SeaGrant_Proto.store.Distance');
 
-	 var store;
+    var store;
  
     beforeEach(function() {
-        //Set up globals
-        // console.log('in before each function');
-		jasmine.Ajax.install();
-        // console.log('Ajax is installed');
-        // for some reason this line doesn't work and store is not created.
-		store = Ext.create('SeaGrant_Proto.store.Education');
-        // console.log('store created');
-        // console.log(store);
+	jasmine.Ajax.install();
+	store = Ext.create('SeaGrant_Proto.store.Education');
     });
 
     afterEach(function() {
-        //Take down globals
-
-        //This looks like it should be leaky, check the sencha API for
-        //something like the model.destroy() function.
-		jasmine.Ajax.uninstall();
+	jasmine.Ajax.uninstall();
         store = null;
-        // console.log('store removed');
     });
 
-	it('exists', function() {
-        // console.log('In exists function');
-        // console.log(store);
+    it('exists', function() {
         expect(store.$className).toEqual('SeaGrant_Proto.store.Education');
     });
 
     it('Is populated with education data', function(){
-        // console.log(store.data);
         expect(store.data.items[0].data.text).toEqual('Ed item 1');
     });
 });
--- tests/jasmine-standalone/spec/javascripts/store/vendorInventorySpec.js
+++ tests/jasmine-standalone/spec/javascripts/store/vendorInventorySpec.js
@@ -1,38 +1,22 @@
 describe('SeaGrant_Proto.store.VendorInventory',function() {
 
-    // Ext.require('SeaGrant_Proto.store.Distance');
-
-	 var store;
+    var store;
  
     beforeEach(function() {
-        //Set up globals
-        // console.log('in before each function');
-		jasmine.Ajax.install();
-        // console.log('Ajax is installed');
-        // for some reason this line doesn't work and store is not created.
-		store = Ext.create('SeaGrant_Proto.store.VendorInventory');
-        // console.log('store created');
-        // console.log(store);
+	jasmine.Ajax.install();
+	store = Ext.create('SeaGrant_Proto.store.VendorInventory');
     });
 
     afterEach(function() {
-        //Take down globals
-
-        //This looks like it should be leaky, check the sencha API for
-        //something like the model.destroy() function.
-		jasmine.Ajax.uninstall();
+	jasmine.Ajax.uninstall();
         store = null;
-        // console.log('store removed');
     });
 
 	it('exists', function() {
-        // console.log('In exists function');
-        // console.log(store);
         expect(store.$className).toEqual('SeaGrant_Proto.store.VendorInventory');
     });
 
     it('Is populated with first test item for vendorInventory', function(){
-        // console.log(store.data);
         expect(store.data.items[0].data.name).toEqual("test Fish");
     });
 });
