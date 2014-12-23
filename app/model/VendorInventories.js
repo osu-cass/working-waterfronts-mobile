c9c86b11712def1c78929b01b71b6506877b726d
Rob Mac <maccelerated@gmail.com>
2014-12-22 16:13:50 -0800

    remove irrelevant models, stores
    Before this commit, the repo is the same as SeaGrant_Proto.
    After this commit, 4 stores and 3 models not applicable to Working
    Waterfronts are removed. Jasmine SpecRunner is updated to not include
    the related unit tests.

--- app/model/VendorInventories.js
+++
@@ -1,9 +0,0 @@
-Ext.define('WorkingWaterfronts.model.VendorInventories', {
-    extend: 'Ext.data.Model',
-    config: {
-        fields:[
-            'name',
-            'preparation'
-        ]
-    }
-});
