dbf19f18b04a364c8549b0415dd76be0b1350d6a
Rob Mac <maccelerated@gmail.com>
2015-01-14 14:39:23 -0800

    add 'navigate' button to PointOfInterest view
    Before this commit, the POI view had no links to external apps. This
    commit adds a button to the POI view, and adds the related handler in
    the view’s controller. This commit also copies the Link utility from
    WhatsFresh into the repo.

---
+++ app/util/Link.js
@@ -0,0 +1,25 @@
+Ext.define('WorkingWaterfronts.util.Link', {
+	singleton: true,
+
+	openNavigation: function(lat, lng){
+		link="daddr="+lat+","+lng;
+		if(navigator.userAgent.match(/(Android)/)){
+			navigator.app.loadUrl("https://maps.google.com/?"+link, {openExternal: true});
+		}else if(navigator.userAgent.match(/(ios)/)){
+			window.open("maps:"+link);
+		}else{
+			window.open("https://maps.google.com/?"+link);
+		}
+	},
+
+	openVideo: function(link){
+		if(navigator.userAgent.match(/(Android)/)){
+			navigator.app.loadUrl(link, {openExternal: true});
+		}else if(navigator.userAgent.match(/(ios)/)){
+			window.open(link);
+		}else{
+			window.open(link);
+		}
+	}
+
+});
