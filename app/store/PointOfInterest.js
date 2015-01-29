51a2f5db6430f1ffd85def56531090e9ba65652f
Rob Mac <maccelerated@gmail.com>
2015-01-29 15:25:43 -0800

    Add fake points of interest
    Rename the production version of the PointOfInterest store so that we
    may have some points to demo.

--- app/store/PointOfInterest.js
+++ app/store/PointOfInterest.js
@@ -2,18 +2,70 @@ Ext.define('WorkingWaterfronts.store.PointOfInterest', {
 	extend: 'Ext.data.Store',
 	config: {
 		model: 'WorkingWaterfronts.model.PointsOfInterest',
-		autoLoad: true,
-		proxy: {
-			type: 'ajax',
-			url: 'http://seagrant-staging.osuosl.org/1/pois',
-			noCache: false,
-			pageParam: false,
-			limitParam: false,
-			startParam: false,
-			reader: {
-				type: 'json',
-				rootProperty: 'pois'
-			}
-		}
+		data: [
+	{
+		name					: 'Somewhere Within Reach',
+		description 			: 'Where you are!',
+		// contact
+		email 					: '',
+		website					: '',
+		phone					: '',
+		contact_name			: '',
+		// location
+		street					: '',
+		city					: '',
+		state					: '',
+		zip						: '',
+		lat						: 44.0,
+		lng						: -120.0,
+		location_description	: '',
+		// meta
+		created					: Date.now(),
+		updated					: Date.now(),
+		ext						: ''
+	},
+	{
+		name					: 'The Limit Of Visiblity',
+		description 			: 'About 80km away from Somewhere',
+		// contact
+		email 					: '',
+		website					: '',
+		phone					: '',
+		contact_name			: '',
+		// location
+		street					: '',
+		city					: 'Coos Bay',
+		state					: '',
+		zip						: '',
+		lat						: 44.0,
+		lng						: -121.0,
+		location_description	: '',
+		// meta
+		created					: Date.now(),
+		updated					: Date.now(),
+		ext						: ''
+	},
+	{
+		name					: 'A Place Beyond Sight',
+		description 			: 'About 136km away from Somewhere',
+		// contact
+		email 					: '',
+		website					: '',
+		phone					: '',
+		contact_name			: '',
+		// location
+		street					: '',
+		city					: 'Newport',
+		state					: '',
+		zip						: '',
+		lat						: 45.0,
+		lng						: -121.0,
+		location_description	: '',
+		// meta
+		created					: Date.now(),
+		updated					: Date.now(),
+		ext						: ''
+	}
+]
 	}
 });
