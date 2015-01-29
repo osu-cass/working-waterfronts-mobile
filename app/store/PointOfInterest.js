4217d61a026bd586fbe1b52d722bc5bfa8f4062c
Rob Mac <maccelerated@gmail.com>
2015-01-29 15:37:48 -0800

    Add additional points of interest
    This commit recreates the demo points with more themed versions.

--- app/store/PointOfInterest.js
+++ app/store/PointOfInterest.js
@@ -3,69 +3,61 @@ Ext.define('WorkingWaterfronts.store.PointOfInterest', {
 	config: {
 		model: 'WorkingWaterfronts.model.PointsOfInterest',
 		data: [
-	{
-		name					: 'Somewhere Within Reach',
-		description 			: 'Where you are!',
-		// contact
-		email 					: '',
-		website					: '',
-		phone					: '',
-		contact_name			: '',
-		// location
-		street					: '',
-		city					: '',
-		state					: '',
-		zip						: '',
-		lat						: 44.0,
-		lng						: -120.0,
-		location_description	: '',
-		// meta
-		created					: Date.now(),
-		updated					: Date.now(),
-		ext						: ''
-	},
-	{
-		name					: 'The Limit Of Visiblity',
-		description 			: 'About 80km away from Somewhere',
-		// contact
-		email 					: '',
-		website					: '',
-		phone					: '',
-		contact_name			: '',
-		// location
-		street					: '',
-		city					: 'Coos Bay',
-		state					: '',
-		zip						: '',
-		lat						: 44.0,
-		lng						: -121.0,
-		location_description	: '',
-		// meta
-		created					: Date.now(),
-		updated					: Date.now(),
-		ext						: ''
-	},
-	{
-		name					: 'A Place Beyond Sight',
-		description 			: 'About 136km away from Somewhere',
-		// contact
-		email 					: '',
-		website					: '',
-		phone					: '',
-		contact_name			: '',
-		// location
-		street					: '',
-		city					: 'Newport',
-		state					: '',
-		zip						: '',
-		lat						: 45.0,
-		lng						: -121.0,
-		location_description	: '',
-		// meta
-		created					: Date.now(),
-		updated					: Date.now(),
-		ext						: ''
-	}
-]
+{
+	name					: 'Pony Point',
+	description 			: 'I\'m not sure what is here.',
+	contact_name			: 'Southwest Oregon Regional Airport',
+	city					: 'Coos Bay',
+	state					: 'Oregon',
+	zip						: '97459',
+	lat						: 43.423016,
+	lng						: -124.240287,
+	location_description	: 'Lookout at the edge of the airport runway.',
+	// meta
+	created					: Date.now(),
+	updated					: Date.now()
+},
+{
+	name					: 'Simpson Park',
+	description 			: 'Sounds like a nice place.',
+	city					: 'Coos Bay',
+	state					: 'Oregon',
+	zip						: '97459',
+	lat						: 43.416938,
+	lng						: -124.225331,
+	location_description	: 'Near the Coos Historical & Maritime Mueseum',
+	// meta
+	created					: Date.now(),
+	updated					: Date.now()
+},
+{
+	name					: 'Conde B McCullough State Ways',
+	description 			: 'I don\'t know what a "state ways" is.',
+	city					: 'North Bend',
+	state					: 'Oregon',
+	zip						: '97459',
+	lat						: 43.444310,
+	lng						: -124.213401,
+	location_description	: 'Along N Bay Rd.',
+	// meta
+	created					: Date.now(),
+	updated					: Date.now(),
+	ext						: ''
+},
+{
+	name					: 'Yoakam Point State Park',
+	description 			: 'Too many descriptions.',
+	city					: 'Coos Bay',
+	state					: 'Oregon',
+	zip						: '97420',
+	lat						: 43.430464,
+	lng						: -124.359816,
+	location_description	: 'Visit Yoakam Point too!',
+	// meta
+	created					: Date.now(),
+	updated					: Date.now(),
+	ext						: ''
+}
+		]
 	}
 });
